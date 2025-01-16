79b830285c358a13db8f90e6b2c692095f4fd434
Jeff Cash <tiborg.iot@gmail.com>
2025-01-15 21:24:20 -0500

    v3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736992503|31940332';
+const CACHE_VERSION = '1736994248|1776545483';
 /** @type {string} */
 const CACHE_PREFIX = 'Cosmic Catch-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
